99c21b4040b44dd803635833820fbd66fa8c2494
rosie-faulkner <rosie.faulkner@mailchimp.com>
2022-11-26 16:27:51 -0500

    news source population and card styling

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
-export const NewsCard = () => {
+export const NewsCard = ({ article }) => {
   return (
-    <div>NewsCard</div>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={article.urlToImage}
+          alt={article.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {article.title}
+            {console.log(article.author)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {article.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { NewsCard } from "../NewsCard/NewsCard";
+import { Grid, Grow } from "@material-ui/core";
 
 const NewsCards = ({ articles }) => {
   return (
-    <div>
-      {articles.map((article, i) => (
-        <NewsCard />
-      ))}
-    </div>
+    <Grow in>
+      <Grid container alignItems="stretch" spacing={3}>
+        {console.log(articles)}
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
+            <NewsCard article={article} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 

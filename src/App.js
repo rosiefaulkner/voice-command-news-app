49cdd3f3f509ab114fafefe67698792dcf35ad80
rosie-faulkner <rosie.faulkner@mailchimp.com>
2022-11-26 01:02:53 -0500

    resolves bug with headers in alan ai request

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey =
-  "7ab64f3faef4d1d185a1b4dd66d16e612e956eca572e1d8b807a3e2338fdd0dc/stage";
+process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
   useEffect(() => {

965558f9fcba2abba6584ff1281bfa8de41dd39b
Rosie Faulkner <54520871+rosiefaulkner@users.noreply.github.com>
2022-11-26 22:39:41 -0500

    alan ai integration JS

---
+++ src/news_reader_app.js
@@ -0,0 +1,88 @@
+// Use this sample to create your own voice commands
+// intent('hello world', p => {
+//     p.play('(hello|hi there)');
+// });
+intent('What does this app do?', 'Whast can I do here?', 
+      reply('This is a news project'));
+
+// intent('Start a command', (p) => {
+//     p.play({command: 'testCommand'})
+// })
+
+const API_KEY = '789d1b502a304499b839ceda86e04cee';
+let savedArticles = [];
+
+// News by Source
+intent('Give me the news from $(source* (.*))', (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines`;
+    
+    if(p.source.value) {
+        p.source.value = p.source.value.toLowerCase().split(" ").join("-");
+        NEWS_API_URL = `${NEWS_API_URL}?sources=${p.source.value}&apiKey=${API_KEY}`
+    }
+    
+    api.request(NEWS_API_URL, {headers: {"user-agent": 'user agent' }}, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length) {
+            p.play(`Sorry, I\'m having trouble finding news from ${p.source.value}. Please try searching for news from somewhere else.`);
+            return;
+        }
+        
+        savedArticles = articles;
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|most recent) headlines from ${p.source.value}.`);
+    });
+})
+
+// News by Term
+intent('What\'s up with $(term* (.*))', (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/everything`;
+    
+    if(p.term.value) {
+        p.term.value = p.term.value.toLowerCase().split(" ").join("-");
+        NEWS_API_URL = `${NEWS_API_URL}?q=${p.term.value}&apiKey=${API_KEY}`
+    }
+    
+    api.request(NEWS_API_URL, {headers: {"user-agent": 'user agent' }}, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length) {
+            p.play(`I can\'t find any news about ${p.term.value}. Look for news about something else.`);
+            return;
+        }
+        
+        savedArticles = articles;
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|most recent) headlines about ${p.term.value}.`);
+    });
+})
+
+// News by Category
+const CATEGORIES = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+const CATEGORIES_INTENT = `${CATEGORIES.map((category) => `${category}~${category}`).join('|')}`;
+
+intent(`(show|what is|tell me|what's|what are|what're|read) (the|) (recent|latest|) $(N news|headlines) (in|about|on|) $(C~ ${CATEGORIES_INTENT})`,
+  `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`, (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines`;
+    
+    if(p.C.value) {
+        NEWS_API_URL = `${NEWS_API_URL}?category=${p.C.value}&apiKey=${API_KEY}&country=us`
+    }
+    
+    api.request(NEWS_API_URL, {headers: {"user-agent": 'user agent' }}, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length) {
+            p.play(`I can\'t find any news about ${p.C.value}. Look for a different category.`);
+            return;
+        }
+        
+        savedArticles = articles;
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|most recent) articles about ${p.C.value}.`);
+    });
+})

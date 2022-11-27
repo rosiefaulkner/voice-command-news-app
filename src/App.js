acf7d35b08b00b523dfbfdf8a73c7641a52b8a4a
rosie-faulkner <rosie.faulkner@mailchimp.com>
2022-11-26 22:07:49 -0500

    adds banner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React, { useState, useEffect } from "react";
-import alanBtn from "@alan-ai/alan-sdk-web";
-import NewsCards from "./components/NewsCards/NewsCards";
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import logo from './images/banner.jpeg';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
 
 const alanKey =
 process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
-        if(command === 'newHeadlines'){
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
       },
     });
   }, []);
+
   return (
     <div>
-      <h1>Alan AI News Application</h1>
-      <NewsCards articles={newsArticles} />
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src={logo} className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/rosiefaulkner/" target="_blank" rel="noreferrer"> Rosie Faulkner</a>
+          </Typography>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -36,7 +36,7 @@ const NewsCards = ({ articles, activeArticle }) => {
     <Grow in>
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {articles.map((article, i) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+          <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
             <NewsCard activeArticle={activeArticle} i={i} article={article} />
           </Grid>
         ))}
